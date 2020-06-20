fd76f47d1d47b53e7dfd386f49e823d5eddedfb3
Samuel Dezso Dysch <sdysch94@gmail.com>
2020-06-20 23:50:44 +0100

    Firefox tag

--- config.def.h
+++ config.def.h
@@ -36,7 +36,7 @@ static const Rule rules[] = {
 	 *	WM_NAME(STRING) = title
 	 */
 	/* class           instance    title       tags mask     isfloating   monitor */
-	{ "Firefox",        NULL,       NULL,       3,            0,           -1 },
+	{ "Firefox",        NULL,       NULL,       1 << 2,            0,           -1 },
 	{ "Google-chrome",  NULL,       NULL,       1,            0,           -1 },
 	{ "Skype",			NULL,       NULL,       1,            0,           0 },
 
